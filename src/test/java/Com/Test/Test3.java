6a0cff92b2a68c8bbf496c91760415e2faf7636b
Mamtabarge <mamata161096@gmail.com>
2022-10-18 17:33:00 +0530

    test3 added

---
+++ src/test/java/Com/Test/Test3.java
@@ -0,0 +1,14 @@
+package Com.Test;
+
+import org.testng.annotations.Test;
+
+import Com.Utility.BaseClass;
+
+public class Test3 extends BaseClass  {
+	
+	@Test
+	public void verifytest3() {
+		System.out.println("Test 3");
+	}
+
+}
